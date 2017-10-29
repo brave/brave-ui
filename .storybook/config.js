b1b71393d429349243916e13b34e35edc2381611
Cezar Augusto <cezaraugusto@outlook.it>
2017-10-29 05:12:08 -0200

    initial setup

---
+++ .storybook/config.js
@@ -0,0 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+ import { configure } from '@storybook/react';
+
+function loadStories() {
+  const req = require.context('../stories', true, /\.js$/);
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ stories/browserButtonStory.js
@@ -0,0 +1,70 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Storybook requires
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+// Components
+import React from 'react'
+import BrowserButton from '../brave-ui/browserButton'
+
+storiesOf('BrowserButton', module)
+  .add('Standard default button', () =>
+    <BrowserButton
+      as='default'
+      label='A default button®'
+      onClick={action('clicked the default button')} />
+  )
+
+  .add('Standard default button disabled', () =>
+    <BrowserButton
+      as='default'
+      disabled
+      label='A default disabled button®' />
+  )
+
+  .add('Standard primary button', () => (
+    <BrowserButton
+      as='primary'
+      label='A primary button®'
+      onClick={action('clicked the primary button')} />
+  ))
+
+  .add('Standard primary button disabled', () =>
+    <BrowserButton
+      as='primary'
+      disabled
+      label='A primary disabled button®' />
+  )
+
+  .add('Standard secondary button', () => (
+    <BrowserButton
+      as='secondary'
+      label='A secondary button®'
+      onClick={action('clicked the secondary button')} />
+  ))
+
+  .add('Standard secondary button disabled', () =>
+    <BrowserButton
+      as='secondary'
+      disabled
+      label='A secondary disabled button®' />
+  )
+
+  .add('Custom size', () => (
+    <BrowserButton
+      as='primary'
+      size='60px'
+      label='A custom size button®'
+      onClick={action('clicked the custom font size button')} />
+  ))
+
+  .add('Custom font size', () => (
+    <BrowserButton
+      as='primary'
+      fontSize='40px'
+      label='A custom font size button®'
+      onClick={action('clicked the custom font size button')} />
+  ))
