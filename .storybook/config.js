013f277fd815a0905b776686df9501f909bb3827
Cezar Augusto <cezaraugusto@outlook.it>
2017-10-29 06:24:36 -0200

    add vscode setting for standard

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- import { configure } from '@storybook/react'
+import {configure} from '@storybook/react'
 
 function loadStories () {
   const req = require.context('../stories', true, /\.js$/)
