574374b70d4e0448e6ee4ea28232d11d9276c598
Cezar Augusto <cezaraugusto@outlook.it>
2017-10-30 08:18:22 -0200

    fix module capitalization in dist

--- brave-ui/dist/index.js
+++ brave-ui/dist/index.js
@@ -5,12 +5,12 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.BrowserButton = undefined;
 
-var _BrowserButton = require('./BrowserButton');
+var _browserButton = require('./browserButton');
 
-var _BrowserButton2 = _interopRequireDefault(_BrowserButton);
+var _browserButton2 = _interopRequireDefault(_browserButton);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.BrowserButton = _BrowserButton2.default; /* This Source Code Form is subject to the terms of the Mozilla Public
+exports.BrowserButton = _browserButton2.default; /* This Source Code Form is subject to the terms of the Mozilla Public
                                                   * License, v. 2.0. If a copy of the MPL was not distributed with this file,
                                                   * You can obtain one at http://mozilla.org/MPL/2.0/. */
