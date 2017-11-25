fcbe645d4ef2314d905e4fdad7c79210b8254685
Cezar Augusto <cezaraugusto@outlook.it>
2017-11-25 21:49:58 -0200

    add an option to build continuously - Useful to test components in a real-life app while developing

--- tests/lib/utils.js
+++ tests/lib/utils.js
@@ -2,14 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import {StyleSheetTestUtils} from 'aphrodite'
+// We need to clear buffer of the Aphrodite dep inside brave-ui package
+// to avoid conflicts when you have both deps installed leading to failing tests
+import {StyleSheetTestUtils as StyleSheetTestUtilsGlobal} from 'aphrodite'
+import {StyleSheetTestUtils as StyleSheetTestUtilsLocal} from '../../brave-ui/node_modules/aphrodite'
 
 const resumeStyleInjection = () => {
   // Aphrodite injects styles async immediately after render().
   // If jest test instances are created before style injections,
   // our tests will fail as `[data-aphrodite]` selector is not yet ready
   return new Promise(resolve => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection()
+    StyleSheetTestUtilsGlobal.clearBufferAndResumeStyleInjection()
+    StyleSheetTestUtilsLocal.clearBufferAndResumeStyleInjection()
     return process.nextTick(resolve)
   })
 }
