c15495abd22e36c4f5bab95afb62b6905ee69fd2
Cezar Augusto <cezaraugusto@outlook.it>
2017-11-28 21:37:13 -0200

    add browserText component

--- brave-ui/dist/index.js
+++ brave-ui/dist/index.js
@@ -3,7 +3,11 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Column = exports.Grid = exports.SwitchButton = exports.BrowserButton = exports.ActionButton = exports.Anchor = undefined;
+exports.Column = exports.Grid = exports.SwitchButton = exports.BrowserButton = exports.ActionButton = exports.Anchor = exports.BrowserText = undefined;
+
+var _browserText = require('./browserText');
+
+var _browserText2 = _interopRequireDefault(_browserText);
 
 var _anchor = require('./anchor');
 
@@ -25,11 +29,14 @@ var _gridSystem = require('./gridSystem');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+exports.BrowserText = _browserText2.default;
 exports.Anchor = _anchor2.default;
 exports.ActionButton = _actionButton2.default;
 exports.BrowserButton = _browserButton2.default;
 exports.SwitchButton = _switchButton2.default;
 exports.Grid = _gridSystem.Grid;
-exports.Column = _gridSystem.Column; /* This Source Code Form is subject to the terms of the Mozilla Public
-                                      * License, v. 2.0. If a copy of the MPL was not distributed with this file,
-                                      * You can obtain one at http://mozilla.org/MPL/2.0/. */
+exports.Column = _gridSystem.Column;
--- brave-ui/src/index.js
+++ brave-ui/src/index.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import BrowserText from './browserText'
 import Anchor from './anchor'
 import ActionButton from './actionButton'
 import BrowserButton from './browserButton'
@@ -9,6 +10,7 @@ import SwitchButton from './switchButton'
 import {Grid, Column} from './gridSystem'
 
 export {
+  BrowserText,
   Anchor,
   ActionButton,
   BrowserButton,
