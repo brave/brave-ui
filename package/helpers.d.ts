da511742c331468f946c9f9090bcc02f189815f4
Cezar Augusto <boss@cezaraugusto.net>
2018-04-19 11:59:38 -0300

    remove legacy build files

--- package/helpers.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ApplyClass {
-    [key: string]: boolean;
-}
-export declare const applyClass: (classesObj: ApplyClass) => string;
--- package/helpers.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.applyClass = (classesObj) => {
-    return Object.keys(classesObj).filter((prop) => classesObj[prop]).join(' ');
-};
-//# sourceMappingURL=helpers.js.map
