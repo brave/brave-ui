7e8790164e78110cddf30df4d4490b3cebee6751
Cezar Augusto <boss@cezaraugusto.net>
2018-05-13 22:44:52 -0300

    remove build files from git

--- package/separator/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-export interface SeparatorProps {
-    noMargin?: boolean;
-}
-declare class Separator extends React.PureComponent<SeparatorProps, {}> {
-    render(): JSX.Element;
-}
-export default Separator;
