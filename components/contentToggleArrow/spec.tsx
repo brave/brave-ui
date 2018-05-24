10eb1085b1c235fb71038ee9ded728b732c51a86
Cezar Augusto <boss@cezaraugusto.net>
2018-05-24 05:25:40 -0300

    update spec of ContentToggleArrow for parity

--- components/contentToggleArrow/spec.tsx
+++ components/contentToggleArrow/spec.tsx
@@ -9,66 +9,70 @@ describe('contentToggleArrow tests', () => {
     <ContentToggleArrow id='testContentToggleArrow' summary='testSummary' {...props} />
   )
 
-  it('matches the snapshot', () => {
-    const component = baseComponent()
-    const tree = create(component).toJSON()
-    expect(tree).toMatchSnapshot()
+  describe('basic tests', () => {
+    it('matches the snapshot', () => {
+      const component = baseComponent()
+      const tree = create(component).toJSON()
+      expect(tree).toMatchSnapshot()
+    })
+
+    it('renders the component', () => {
+      const wrapper = shallow(baseComponent({id: 'testContentToggleArrowId'}))
+      const assertion = wrapper.find('#testContentToggleArrowId').length
+      expect(assertion).toBe(1)
+    })
   })
 
-  it('renders the component', () => {
-    const wrapper = shallow(baseComponent({id: 'testContentToggleArrowId'}))
-    const assertion = wrapper.find('#testContentToggleArrowId').length
-    expect(assertion).toBe(1)
-  })
+  describe('component behavior', () => {
+    it('can have an id', () => {
+      const wrapper = shallow(baseComponent({id: 'coolId'}))
+      const assertion = wrapper.props().id
+      expect(assertion).toBe('coolId')
+    })
 
-  it('can have an id', () => {
-    const wrapper = shallow(baseComponent({id: 'coolId'}))
-    const assertion = wrapper.props().id
-    expect(assertion).toBe('coolId')
-  })
+    it('can have a summary', () => {
+      const wrapper = shallow(baseComponent({summary: 'testSummary'}))
+      const assertion = wrapper.html().includes('testSummary')
+      expect(assertion).toBe(true)
+    })
 
-  it('can have a summary', () => {
-    const wrapper = shallow(baseComponent({summary: 'testSummary'}))
-    const assertion = wrapper.html().includes('testSummary')
-    expect(assertion).toBe(true)
-  })
+    it('can be opened', () => {
+      const wrapper = shallow(baseComponent({open: true}))
+      const assertion = wrapper.props().open
+      expect(assertion).toBe(true)
+    })
 
-  it('can be opened', () => {
-    const wrapper = shallow(baseComponent({open: true}))
-    const assertion = wrapper.props().open
-    expect(assertion).toBe(true)
-  })
+    it('can be closed', () => {
+      const wrapper = shallow(baseComponent({open: false}))
+      const assertion = wrapper.props().open
+      expect(assertion).toBe(false)
+    })
 
-  it('can be closed', () => {
-    const wrapper = shallow(baseComponent({open: false}))
-    const assertion = wrapper.props().open
-    expect(assertion).toBe(false)
-  })
+    it('can be set as default to open', () => {
+      const wrapper = shallow(baseComponent({defaultOpen: true}))
+      const assertion = wrapper.props().open
+      expect(assertion).toBe(true)
+    })
 
-  it('can be set as default to open', () => {
-    const wrapper = shallow(baseComponent({defaultOpen: true}))
-    const assertion = wrapper.props().open
-    expect(assertion).toBe(true)
-  })
+    it('can have a separator included', () => {
+      const wrapper = shallow(baseComponent({withSeparator: true}))
+      const assertion = wrapper.props().withSeparator
+      expect(assertion).toBe(true)
+    })
 
-  it('can have a separator included', () => {
-    const wrapper = shallow(baseComponent({withSeparator: true}))
-    const assertion = wrapper.props().withSeparator
-    expect(assertion).toBe(true)
-  })
+    it('can set its own state to open', () => {
+      const wrapper = shallow(baseComponent())
+      wrapper.setProps({open: true})
+      const assertion = wrapper.state('open')
+      expect(assertion).toBe(true)
+    })
 
-  it('can set its own state to open', () => {
-    const wrapper = shallow(baseComponent())
-    wrapper.setProps({open: true})
-    const assertion = wrapper.state('open')
-    expect(assertion).toBe(true)
-  })
-
-  it('can respond to click', () => {
-    const value = {target: { open: true }}
-    const onClick = jest.fn()
-    const wrapper = shallow(baseComponent({onClick}))
-    wrapper.find('#testContentToggleArrowControl').simulate('click', value)
-    expect(onClick).toBeCalledWith(value)
+    it('can respond to click', () => {
+      const value = {target: { open: true }}
+      const onClick = jest.fn()
+      const wrapper = shallow(baseComponent({onClick}))
+      wrapper.find('#testContentToggleArrowControl').simulate('click', value)
+      expect(onClick).toBeCalledWith(value)
+    })
   })
 })
