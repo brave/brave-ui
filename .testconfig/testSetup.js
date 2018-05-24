82a23b1097e6a0061472263df4beaa59f89bc15c
Cezar Augusto <boss@cezaraugusto.net>
2018-05-24 02:19:06 -0300

    use jest-styled-components closes #32

--- .testconfig/testSetup.js
+++ .testconfig/testSetup.js
@@ -4,6 +4,7 @@
 
 const Enzyme = require('enzyme')
 const EnzymeAdapter = require('enzyme-adapter-react-16')
+require('jest-styled-components')
 
 // Setup enzyme's react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() })
