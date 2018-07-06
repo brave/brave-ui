5fafa2e5c45fc9e7dd5cd99241fff919afcdca03
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-07-06 12:17:07 +0200

    Add modal

---
+++ components/rewards/modal/index.tsx
@@ -0,0 +1,45 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import * as React from 'react'
+import { StyledWrapper, StyledDialog, StyledClose, StyledContent } from './style'
+
+export interface Props {
+  id?: string
+  maxWidth?: string
+  outsideClose?: boolean
+  children?: React.ReactNode
+  onClose?: () => void
+}
+
+const close = require('./assets/close')
+
+export default class Modal extends React.PureComponent<Props, {}> {
+  outsideClose = () => {
+    if (!this.props.outsideClose) {
+      return
+    }
+
+    if (this.props.onClose) {
+      this.props.onClose()
+    }
+  }
+
+  render () {
+    const { id, onClose, maxWidth, children } = this.props
+
+    return (
+      <StyledWrapper id={id} onClick={this.outsideClose}>
+        <StyledDialog maxWidth={maxWidth}>
+          <StyledClose onClick={onClose}>
+            {close}
+          </StyledClose>
+          <StyledContent>
+            {children}
+          </StyledContent>
+        </StyledDialog>
+      </StyledWrapper>
+    )
+  }
+}
