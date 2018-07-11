7b72bb7fcaf62d796dce2eda70873e94a198317d
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-07-11 08:13:31 +0200

    Add exports

--- components/index.ts
+++ components/index.ts
@@ -19,6 +19,42 @@ import Page from './page'
 import { DataBlock, DataItem } from './dataBlock'
 import Clock from './clock'
 
+// Rewards
+import Alert from './rewards/alert'
+import ButtonPrimary from './rewards/buttonPrimary'
+import DisabledContent from './rewards/disabledContent'
+import MainToggle from './rewards/mainToggle'
+import PanelEmpty from './rewards/panelEmpty'
+import SiteBanner from './rewards/siteBanner'
+import Toggle from './rewards/toggle'
+import Amount from './rewards/amount'
+import ButtonSecondary from './rewards/buttonSecondary'
+import Donate from './rewards/donate'
+import Modal from './rewards/modal'
+import PanelOff from './rewards/panelOff'
+import Table from './rewards/table'
+import Tokens from './rewards/tokens'
+import Box from './rewards/box'
+import Checkbox from './rewards/checkbox'
+import DonationTable from './rewards/donationTable'
+import ModalBackupRestore from './rewards/modalBackupRestore'
+import PanelSummary from './rewards/panelSummary'
+import Tabs from './rewards/tabs'
+import Tooltip from './rewards/tooltip'
+import ButtonCta from './rewards/buttonCta'
+import ContributeTable from './rewards/contributeTable'
+import List from './rewards/list'
+import ModalContribute from './rewards/modalContribute'
+import Profile from './rewards/profile'
+import Textarea from './rewards/textarea'
+import ButtonGhost from './rewards/buttonGhost'
+import ControlWrapper from './rewards/controlWrapper'
+import ListToken from './rewards/listToken'
+import Panel from './rewards/panel'
+import Select from './rewards/select'
+import Tip from './rewards/tip'
+
+
 export {
   TextLabel,
   Anchor,
@@ -40,5 +76,38 @@ export {
   Page,
   DataBlock,
   DataItem,
-  Clock
+  Clock,
+  Alert,
+  ButtonPrimary,
+  DisabledContent,
+  MainToggle,
+  PanelEmpty,
+  SiteBanner,
+  Toggle,
+  Amount,
+  ButtonSecondary,
+  Donate,
+  Modal,
+  PanelOff,
+  Table,
+  Tokens,
+  Box,
+  Checkbox,
+  DonationTable,
+  ModalBackupRestore,
+  PanelSummary,
+  Tabs,
+  Tooltip,
+  ButtonCta,
+  ContributeTable,
+  List,
+  ModalContribute,
+  Profile,
+  Textarea,
+  ButtonGhost,
+  ControlWrapper,
+  ListToken,
+  Panel,
+  Select,
+  Tip
 }
