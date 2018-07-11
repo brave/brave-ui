5caca3894525ed63f2be77d13dc68dff9f029dd8
Cezar Augusto <boss@cezaraugusto.net>
2018-07-11 15:45:45 -0300

    add panel component

---
+++ package/v1/panel/index.d.ts
@@ -0,0 +1,27 @@
+/// <reference types="react" />
+import * as React from 'react';
+export interface PanelTheme {
+    display?: string;
+    flexDirection?: string;
+    justifyContent?: string;
+    alignItems?: string;
+    maxWidth?: string;
+    minWidth?: string;
+    width?: string;
+    height?: string;
+    minHeight?: string;
+    margin?: string;
+    padding?: string;
+    backgroundColor?: string;
+    fontFamily?: string;
+    color?: string;
+}
+export interface PanelProps {
+    id?: string;
+    theme?: PanelTheme;
+    children?: React.ReactNode;
+}
+declare class Panel extends React.PureComponent<PanelProps, {}> {
+    render(): JSX.Element;
+}
+export default Panel;
