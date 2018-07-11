4d69353008cb7c6440abe67962c96fbe3aea81de
Cezar Augusto <boss@cezaraugusto.net>
2018-07-11 15:45:07 -0300

    add image component

---
+++ package/v1/image/index.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="react" />
+import * as React from 'react';
+export interface ImageTheme {
+    maxWidth?: string;
+    minWidth?: string;
+    width?: string;
+    height?: string;
+    minHeight?: string;
+    margin?: string;
+    padding?: string;
+}
+export interface ImageProps {
+    theme?: ImageTheme;
+    id?: string;
+    children?: HTMLSourceElement;
+    src?: string;
+    caption?: string;
+}
+declare class Image extends React.PureComponent<ImageProps, {}> {
+    render(): JSX.Element;
+}
+export default Image;
---
+++ package/v1/image/style.d.ts
@@ -0,0 +1,5 @@
+declare const StyledPicture: any;
+declare const StyledFigure: any;
+declare const StyledFigcaption: any;
+declare const StyledImage: any;
+export { StyledPicture, StyledFigure, StyledFigcaption, StyledImage };
