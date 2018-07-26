1ba67db0c2603bd08bac913e960823a6e553a921
petemill <miller.pete+commit@gmail.com>
2018-07-26 01:12:55 -0700

    Introduce icons
    These icons are generated via a script (TODO: push icon generation repo) from sketch output and then:
    - Run through SVGO
    - Put in to a template which adds the correct module syntax
    - Auto-lint'ed (TODO: needs adhering to this repo's lint, currently incorrectly using standard)

---
+++ stories/components/icons/icons.tsx
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { storiesOf } from '@storybook/react'
+import * as React from 'react'
+// Components
+import * as AllIcons from '../../../src/components/icons'
+import * as Styled from './style'
+
+const book = storiesOf('Components/Icons', module)
+
+const AllIconComponents: any[] = []
+const AllIconExports: any = AllIcons
+for (const componentName in AllIcons) {
+  const Component = AllIconExports[componentName]
+  AllIconComponents.push({ componentName, Component })
+}
+
+book.add('All Icons', () => {
+  return (
+    <Styled.IconGrid>
+      {AllIconComponents.map(({ componentName, Component }) => (
+          <Styled.IconGridItem title={componentName}>
+            <Styled.IconGridItemIcon><Component /></Styled.IconGridItemIcon>
+            <Styled.IconGridItemTitle>{componentName}</Styled.IconGridItemTitle>
+          </Styled.IconGridItem>
+        ))}
+    </Styled.IconGrid>
+  )
+})
