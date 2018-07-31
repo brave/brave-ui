511cb2f618f50ebfe3253aaed3ac0e9e4394f48c
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-07-31 09:11:24 +0200

    Removes old from default export

--- package/index.d.ts
+++ package/index.d.ts
@@ -1,4 +1,3 @@
-export * from './old';
 export * from './components';
 export * from './features/rewards';
 export { initLocale } from './helpers';
--- package/index.js
+++ package/index.js
@@ -3,7 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./old"));
 __export(require("./components"));
 __export(require("./features/rewards"));
 var helpers_1 = require("./helpers");
