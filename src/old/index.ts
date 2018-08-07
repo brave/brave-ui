29052867c31ce05302ecefe402b5ed04b781132f
NejcZdovc <nejc.zdovc@3zsistemi.si>
2018-08-07 10:36:27 +0200

    Adds missing exports

--- src/old/index.ts
+++ src/old/index.ts
@@ -2,40 +2,40 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import TextLabel from './textLabel'
 import Anchor from './anchor'
-import UnstyledButton from './unstyledButton'
-import PushButton from './pushButton'
-import SwitchButton from './switchButton'
-import SelectOption from './selectOption'
-import ContentToggleArrow from './contentToggleArrow'
-import Separator from './separator'
 import BoxedContent from './boxedContent'
-import Paragraph from './paragraph'
-import MediaContent from './mediaContent'
-import { TitleHeading, SectionHeading, FeatureHeading, Heading } from './headings'
-import Page from './page'
-import { DataBlock, DataItem } from './dataBlock'
 import Clock from './clock'
+import ContentToggleArrow from './contentToggleArrow'
+import MediaContent from './mediaContent'
+import Page from './page'
+import Paragraph from './paragraph'
+import PushButton from './pushButton'
+import SelectOption from './selectOption'
+import Separator from './separator'
+import SwitchButton from './switchButton'
+import TextLabel from './textLabel'
+import UnstyledButton from './unstyledButton'
+import { DataBlock, DataItem } from './dataBlock'
+import { TitleHeading, SectionHeading, FeatureHeading, Heading } from './headings'
 
 export {
-  TextLabel,
   Anchor,
-  UnstyledButton,
-  PushButton,
-  SwitchButton,
-  SelectOption,
-  ContentToggleArrow,
-  Separator,
   BoxedContent,
-  Paragraph,
-  MediaContent,
-  TitleHeading,
-  SectionHeading,
-  FeatureHeading,
-  Heading,
-  Page,
+  Clock,
+  ContentToggleArrow,
   DataBlock,
   DataItem,
-  Clock
+  FeatureHeading,
+  Heading,
+  MediaContent,
+  Page,
+  Paragraph,
+  PushButton,
+  SectionHeading,
+  SelectOption,
+  Separator,
+  SwitchButton,
+  TextLabel,
+  TitleHeading,
+  UnstyledButton
 }
