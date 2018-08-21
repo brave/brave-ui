66ce9ddf3e6560d776c2aa928e43652d1fb2c168
petemill <miller.pete+commit@gmail.com>
2018-08-20 20:25:59 -0700

    Add Theme interface and first implementation for Brave-Default (light) theme
    Also adds a palette file which all (most) theme color values should come from, and any non-theme direct color values (which shouldn't happen often!)
    Re-exports styled-components adding on typescript-compatible theme types at /src/theme
    All components now need to be inside a <ThemeProvider> element somewhere in its ancestry. Configures Storybook to wrap all stories in it with default theme. If we have multiple themes, we could provide a switch in Storybook that changes this central location.
    Created a <TestThemeProvider> with a default theme pre-configured, for use in tests.
    
    Adds theme documentation

---
+++ src/theme/brave-default.ts
@@ -0,0 +1,43 @@
+import colors from './palette'
+import ITheme from './theme-interface'
+
+// Define colors, fonts, and sizes by purpose.
+// Keys should not describe the value, but what they are to be used for.
+
+const theme: ITheme = {
+  color: {
+    brandBrave: colors.orange400,
+    brandBat: colors.blurple400,
+    // brave controls
+    brandBraveInteracting: colors.orange500,
+    brandBraveActive: colors.orange200,
+    brandBraveLight: colors.orange000,
+    // bat controls
+    brandBatInteracting: colors.blurple500,
+    brandBatActive: colors.blurple200,
+    // regular controls
+    defaultControl: colors.grey800,
+    defaultControlInteracting: colors.black,
+    defaultControlActive: colors.grey700,
+    // warning controls
+    warn: colors.red500,
+    warnInteracting: colors.red600,
+    warnActive: colors.red300,
+    // subtle controls
+    subtle: colors.grey300,
+    subtleInteracting: colors.grey400,
+    subtleActive: colors.grey200,
+    // disabled controls
+    disabled: colors.grey300,
+    // backgrounds
+    primaryBackground: colors.white,
+    secondaryBackground: colors.grey400
+  },
+  fontFamily: {
+    heading: 'Poppins, sans-serif',
+    body: 'Muli, sans-serif',
+    system: '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif'
+  }
+}
+
+export default theme
---
+++ src/theme/theme-interface.ts
@@ -0,0 +1,32 @@
+import { ThemedStyledProps } from 'styled-components'
+
+export default interface IThemeProps {
+  color: {
+    brandBrave: string,
+    brandBraveInteracting: string,
+    brandBraveActive: string,
+    brandBraveLight: string,
+    brandBat: string,
+    brandBatInteracting: string,
+    brandBatActive: string,
+    disabled: string,
+    primaryBackground: string,
+    secondaryBackground: string,
+    defaultControl: string,
+    defaultControlInteracting: string,
+    defaultControlActive: string
+    warn: string,
+    warnInteracting: string,
+    warnActive: string,
+    subtle: string,
+    subtleInteracting: string,
+    subtleActive: string
+  },
+  fontFamily: {
+    heading: string,
+    body: string,
+    system: string
+  }
+}
+
+export type BraveThemedStyledProps<T> = ThemedStyledProps<T, IThemeProps>
