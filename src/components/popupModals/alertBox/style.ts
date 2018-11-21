f557357d5e100da9cc1c595dac849c4097bd309f
Cezar Augusto <boss@cezaraugusto.net>
2018-11-21 15:21:00 -0200

    add alertbox component

---
+++ src/components/popupModals/alertBox/style.ts
@@ -0,0 +1,36 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License. v. 2.0. If a copy of the MPL was not distributed with this file.
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import styled from 'styled-components'
+
+export const StyledDialog = styled<{}, 'div'>('div')`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 470px;
+  height: 200px;
+  overflow: hidden;
+  margin: auto;
+  padding: 30px 20px 10px;
+  max-height: calc(100vh - 100px);
+  display: grid;
+  grid-template-rows: 1fr auto;
+  overflow-y: auto;
+  background: #fff;
+  box-shadow: 1px 2px 18px 0 rgba(0,0,0,0.5);
+  border-radius: 6px;
+`
+
+export const StyledFooter = styled<{}, 'footer'>('footer')`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 10px;
+`
+
+export const StyledCancelContainer = styled<{}, 'div'>('div')`
+  display: flex;
+  justify-content: flex-end;
+`
