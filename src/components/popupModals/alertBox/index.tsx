80d1a6af51edd02b66348bb8bca9b0dd3cb131e4
Cezar Augusto <boss@cezaraugusto.net>
2019-01-02 20:09:18 -0200

    update alertBox component

--- src/components/popupModals/alertBox/index.tsx
+++ src/components/popupModals/alertBox/index.tsx
@@ -3,7 +3,7 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import * as React from 'react'
-import { StyledDialog, StyledFooter, StyledCancelContainer } from './style'
+import { StyledDialogWrapper, StyledDialog, StyledFooter, StyledCancelContainer } from './style'
 import Button from '../../buttonsIndicators/button'
 
 export interface Props {
@@ -24,6 +24,7 @@ export default class AlertBox extends React.PureComponent<Props, {}> {
     const { testId, children, ...buttonProps } = this.props
     const { colorType, onClickCancel, cancelString, onClickOk, okString } = buttonProps
     return (
+      <StyledDialogWrapper>
       <StyledDialog data-test-id={testId}>
         <div>{children}</div>
         <StyledFooter>
@@ -37,6 +38,7 @@ export default class AlertBox extends React.PureComponent<Props, {}> {
           <Button type={colorType} onClick={onClickOk} text={okString} />
         </StyledFooter>
       </StyledDialog>
+      </StyledDialogWrapper>
     )
   }
 }
