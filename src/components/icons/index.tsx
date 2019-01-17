b46eeb072e4d01e13409fdbb67e8e9ce2c7becda
Cezar Augusto <boss@cezaraugusto.net>
2019-01-17 16:46:11 -0200

    Merge pull request #344 from brave/newtab
    Add support for the default new tab page

--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -18,6 +18,9 @@ export const ArrowLeftIcon = RotatedIconComponent(ArrowIcon, 0)
 export const ArrowRightIcon = RotatedIconComponent(ArrowIcon, 180)
 export const ArrowDownIcon = RotatedIconComponent(ArrowIcon, -90)
 export const ArrowUpIcon = RotatedIconComponent(ArrowIcon, 90)
+export { default as BookmarkBook } from './bookmarkBook'
+export { default as BookmarkIcon } from './bookmark'
+export { default as BookmarkOIcon } from './bookmark-o'
 export const CaratCircleUpIcon = RotatedIconComponent(CaratCircleIcon, 0)
 export const CaratCircleDownIcon = RotatedIconComponent(CaratCircleIcon, 180)
 export const CaratRightIcon = RotatedIconComponent(CaratIcon, 180)
@@ -33,9 +36,11 @@ export { default as CloseStrokeShadowIcon } from './close-shadow'
 export { default as DownloadIcon } from './download'
 export { default as EmoteSadIcon } from './emote-sad'
 export { default as EmoteSmileIcon } from './emote-smile'
+export { default as HistoryIcon } from './history'
 export { default as LogoYoutubeIcon } from './logo-youtube'
 export { default as OpenNewIcon } from './open-new'
 export { default as PaymentDueIcon } from './payment-due'
+export { default as PinIcon } from './pin'
 export { default as PrintIcon } from './print'
 export { default as RefreshIcon } from './refresh'
 export { default as SendIcon } from './send'
