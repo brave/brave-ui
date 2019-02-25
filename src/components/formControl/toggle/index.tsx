25cdabe9420b6eeceadb0e6b10968e7f32a5bce2
Ross Moody <rmoody@brave.com>
2019-02-25 10:46:16 -0800

    Toggle refresh - adjusted styling and general css and linked to theme - removed inline theme reference of 'light'/'dark' - aligned size naming convention to 'default ' | 'small' - additional state polish

--- src/components/formControl/toggle/index.tsx
+++ src/components/formControl/toggle/index.tsx
@@ -1,38 +1,44 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this file,
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
 import * as React from 'react'
-import { StyledWrapper, StyledSlider, StyledBullet, StyleToggle, StyledText } from './style'
-import { getLocale } from '../../../helpers'
+import { StyledWrapper, StyledSlider, StyledBullet, StyleToggle } from './style'
 
 export interface Props {
+  onToggle?: (e: React.MouseEvent<HTMLDivElement>) => void
   id?: string
   disabled?: boolean
   checked?: boolean
-  size?: 'large' | 'small'
-  type?: 'dark' | 'light'
-  onToggle?: (event: React.MouseEvent<HTMLDivElement>) => void
+  size?: 'default' | 'small'
   testId?: string
+  className?: string
 }
 
 export default class Toggle extends React.PureComponent<Props, {}> {
   static defaultProps = {
-    size: 'large',
-    type: 'dark'
+    size: 'default'
   }
 
   render () {
-    const { id, onToggle, disabled, checked, type, size, testId } = this.props
+    const { id, onToggle, disabled, checked, size, testId } = this.props
 
     return (
-      <StyledWrapper id={id}>
-        <StyledText size={size} checked={checked} disabled={disabled}>
-          {getLocale('off')}
-        </StyledText>
-        <StyleToggle data-test-id='toggle' data-test-id2={testId} data-toggled={checked} onClick={!disabled ? onToggle : undefined} size={size}>
-          <StyledSlider size={size} disabled={disabled}/>
-          <StyledBullet size={size} checked={checked} type={type} disabled={disabled} />
+      <StyledWrapper id={id} >
+        <StyleToggle
+          onClick={!disabled ? onToggle : undefined}
+          data-test-id='toggle'
+          data-test-id2={testId}
+          data-toggled={checked}
+          size={size}
+          disabled={disabled}
+        >
+          <StyledSlider
+            size={size}
+            disabled={disabled}
+            checked={checked}
+          />
+          <StyledBullet
+            size={size}
+            checked={checked}
+            disabled={disabled}
+          />
         </StyleToggle>
       </StyledWrapper>
     )
--- src/components/formControl/toggle/style.ts
+++ src/components/formControl/toggle/style.ts
@@ -1,42 +1,83 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License. v. 2.0. If a copy of the MPL was not distributed with this file.
- * You can obtain one at http://mozilla.org/MPL/2.0/. */
-
-import styled, { css } from 'styled-components'
+import styled, { css, ThemedStyledProps } from '../../style/theme'
+import colors from '../../style/color/colorPalette'
 import { Props } from './index'
 
-const getBulletStyle = (p: Props) => {
-  // default is large with dark variation
-  let size = 20
-  let offX = 20
-  let offY = 4
-  let bgColor = '#6D73D2'
-
-  if (p.size === 'small') {
-    size = 16
-    offX = 12
-    offY = 3
+function defaultSmall (
+  defaultValue: any,
+  smallValue: any
+) {
+  return (p: Props) => {
+    switch (p.size) {
+      case 'default':
+        return defaultValue
+      case 'small':
+        return smallValue
+    }
   }
+}
 
-  if (p.type === 'light') {
-    bgColor = '#fb542b'
-  }
+const getTheme = (p: ThemedStyledProps<Props>) => {
+  let bulletColor = '#FFFFFF'
+  let bulletHoverColor = '#FFFFFF'
+  let bulletActiveColor = '#FFFFFF'
+  let sliderColor = colors.grey300
+  let sliderHoverColor = colors.grey400
 
-  if (!p.checked) {
-    offX = -1
-    bgColor = '#CDD1D5'
-  }
-
-  if (p.disabled) {
-    bgColor = '#EBECF0'
+  if (p.checked) {
+    bulletColor = p.theme.color.brand.brave
+    bulletHoverColor = p.theme.color.brand.braveHover
+    bulletActiveColor = p.theme.color.brand.braveActive
   }
 
   return css`
-    --toggle-bullet-size: ${size}px;
-    --toggle-bullet-transform: translate(${offX}px, calc(-50% - ${offY}px));
-    --toggle-bullet-background: ${bgColor};
+  --toggle-bullet-color: ${bulletColor};
+  --toggle-bullet-hoverColor: ${bulletHoverColor};
+  --toggle-bullet-activeColor: ${bulletActiveColor};
+  --toggle-slider-color: ${sliderColor};
+  --toggle-slider-hoverColor: ${sliderHoverColor};
   `
 }
+
+const moveBullet = (p: Props) => {
+  let translate = '22px'
+  if (p.size === 'small') {
+    translate = '16px'
+  }
+  if (!p.checked) {
+    translate = '0'
+  }
+
+  return css`
+  --toggle-bullet-translate: translateX(${translate});
+  `
+}
+
+// TODO: the Focus of this element needs hooked up correctly.
+// The styling is right but tabbing to the element in the dom doesn't work.
+export const StyledBullet = styled<Props, 'div'>('div')`
+  ${getTheme};
+  ${moveBullet};
+  position: absolute;
+  cursor: pointer;
+  height: ${defaultSmall('20px', '16px')};
+  width: ${defaultSmall('20px', '16px')};
+  background-color: var(--toggle-bullet-color);
+  border-radius: 50%;
+  box-shadow: ${(p) => p.theme.shadows.small};
+  transform: var(--toggle-bullet-translate);
+  transition: ${(p) => p.theme.transitionSpeed[0]};
+`
+
+export const StyledSlider = styled<Props, 'div'>('div')`
+  ${getTheme};
+  position: absolute;
+  top: ${defaultSmall('6px', '5px')};
+  height: ${defaultSmall('8px', '6px')};
+  width: ${(p) => p.size === 'small' ? '28px' : '42px'};
+  background-color: var(--toggle-slider-color);
+  border-radius: ${(p) => p.theme.spacing[2]};
+  transition: ${(p) => p.theme.transitionSpeed[0]};
+`
 
 export const StyledWrapper = styled<Props, 'div'>('div')`
   display: flex;
@@ -44,46 +85,23 @@ export const StyledWrapper = styled<Props, 'div'>('div')`
 
 export const StyleToggle = styled<Props, 'div'>('div')`
   position: relative;
-  display: block;
-  height: ${(p) => p.size === 'small' ? '16px' : '24px'};
-  width: ${(p) => p.size === 'small' ? '28px' : '40px'};
-
-  ${(p) => p.disabled
-    ? css`
-      pointer-events: none;
-      animation: none;
-    ` : ''
-  };
-`
-
-export const StyledSlider = styled<Props, 'div'>('div')`
-  background: ${(p) => p.disabled ? '#F6F6FA' : '#A7ACB2'};
-  height: ${(p) => p.size === 'small' ? '6px' : '8px'};
-  margin-top: ${(p) => p.size === 'small' ? '5px' : '8px'};
-  width: 100%;
-  border-radius: 3px;
-`
-
-export const StyledBullet = styled<Props, 'div'>('div')`
-  position: relative;
-  border-radius: 50%;
-  transition: all .4s ease;
-  ${getBulletStyle};
-  width: var(--toggle-bullet-size);
-  height: var(--toggle-bullet-size);
-  transform: var(--toggle-bullet-transform);
-  background-color: var(--toggle-bullet-background);
-  box-shadow: 0 3px 3px rgba(0,0,0,0.05);
-`
-
-export const StyledText = styled<Props, 'div'>('div')`
-  color: #838391;
-  font-size: ${(p) => p.size === 'small' ? '12px' : '14px'};
-  font-family: Poppins, sans-serif;
-  text-align: right;
-  letter-spacing: 0.4px;
-  text-transform: uppercase;
-  margin: ${(p) => p.size === 'small' ? '0px' : '4px'} 8px 0 0;
-  opacity: ${(p) => !p.checked && !p.disabled ? 1 : 0 };
-  transition: 100ms ease-out;
+  display: inline-block;
+  height: ${(p) => p.size === 'small' ? '16px' : '20px'};
+  width: ${(p) => p.size === 'small' ? '28px' : '42px'};
+  transition: ${(p) => p.theme.transitionSpeed[0]};
+  pointer-events: ${(p) => p.disabled ? 'none' : ''};
+  opacity: ${(p) => p.disabled ? '.5' : ''};
+  ${StyledSlider} {
+    background-color: var(--toggle-slider-color);
+  }
+  :hover {
+    ${StyledSlider} {
+      background-color: var(--toggle-slider-hoverColor);
+    }
+  }
+  :active {
+    ${StyledBullet} {
+      background-color: var(--toggle-bullet-activeColor);
+    }
+  }
 `
