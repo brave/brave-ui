4f887bfb5c03ef0960bd5d15617c7acb81c84525
Ross Moody <rmoody@brave.com>
2019-03-02 18:06:15 -0800

    update addon-centered syntax

--- stories/components/popupsModal.tsx
+++ stories/components/popupsModal.tsx
@@ -2,21 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// Storybook requires
+import * as React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs'
-// @ts-ignore
-import centered from '@storybook/addon-centered/dist'
-
-import * as React from 'react'
 import { withState } from '@dump247/storybook-state'
-
-// Components
 import { Modal, AlertBox } from '../../src/components'
 
 storiesOf('Components/Popups and Modals', module)
   .addDecorator(withKnobs)
-  .addDecorator(centered)
   .add('Dialog', withState({ visible: true }, (store) => {
     const onClose = () => {
       store.set({ visible: !store.state.visible })
