b0068ac7c063670672ceb39bf043db9b60b1be52
Ross Moody <rmoody@brave.com>
2019-03-14 13:19:29 -0700

    v5 storybook update

--- stories/components/formControls.tsx
+++ stories/components/formControls.tsx
@@ -18,7 +18,7 @@ import { CheckCircleIcon } from '../../src/components/icons'
 storiesOf('Components/Form controls', module)
   .addDecorator(withKnobs)
   .addDecorator(centered)
-  .add('Wrapper',() => {
+  .add('Wrapper', () => {
     return (
       <ControlWrapper
         text={text('Label', 'Control label')}
@@ -37,15 +37,15 @@ storiesOf('Components/Form controls', module)
 
     return (
       <Toggle
-        size={select('Size', sizeOptions, 'large')}
+        size={select<any>('Size', sizeOptions, 'large')}
         checked={store.state.checked}
-        type={select('Type', { dark: 'dark', light: 'light' }, 'dark')}
+        type={select<any>('Type', { dark: 'dark', light: 'light' }, 'dark')}
         disabled={boolean('Disabled?', false)}
         onToggle={onToggle}
       />
     )
   }))
-  .add('Select',() => {
+  .add('Select', () => {
     return (
       <>
         <div style={{ width: '310px', background: '#fff', padding: '30px' }}>
@@ -129,107 +129,107 @@ storiesOf('Components/Form controls', module)
     )
   })
   .add('Checkbox', withState({ 'yt': true, 'tw': false, 'inst': false }, (store) => {
-    const onChange = (key: string, selected: boolean, child: React.ReactNode, all: {[key: string]: boolean}) => {
+    const onChange = (key: string, selected: boolean, child: React.ReactNode, all: { [key: string]: boolean }) => {
       store.set(all)
     }
     return (
       <>
-       <div style={{ width: '310px', background: '#fff', padding: '30px' }}>
-        <ControlWrapper
-          text={text('Title', 'Enable ability to give tips on ‘Like’ posts')}
-          type={'light'}
-          disabled={boolean('Disabled', false)}
-        >
-          <Checkbox
-            value={object('Checkbox values', store.state)}
-            size={select('Size', { small: 'small', big: 'big' }, 'small')}
-            multiple={boolean('Is multiple?', false)}
-            disabled={boolean('Disabled', false)}
-            onChange={onChange}
+        <div style={{ width: '310px', background: '#fff', padding: '30px' }}>
+          <ControlWrapper
+            text={text('Title', 'Enable ability to give tips on ‘Like’ posts')}
             type={'light'}
-          >
-            <div data-key='yt'>YouTube</div>
-            <div data-key='tw'>Twitter</div>
-            <div data-key='inst'>Instagram Instagram Instagram Instagram Instagram</div>
-          </Checkbox>
-        </ControlWrapper>
-      </div>
-      <div style={{ width: '310px', background: '#4B4C5C', padding: '30px' }}>
-        <ControlWrapper
-          text={text('Title', 'Enable ability to give tips on ‘Like’ posts')}
-          type={'dark'}
-          disabled={boolean('Disabled', false)}
-        >
-          <Checkbox
-            value={object('Checkbox values', store.state)}
-            size={select('Size', { small: 'small', big: 'big' }, 'small')}
-            multiple={boolean('Is multiple?', false)}
             disabled={boolean('Disabled', false)}
-            onChange={onChange}
-            type={'dark'}
           >
-            <div data-key='yt'>YouTube</div>
-            <div data-key='tw'>Twitter</div>
-            <div data-key='inst'>Instagram Instagram Instagram Instagram Instagram</div>
-          </Checkbox>
-        </ControlWrapper>
-      </div>
+            <Checkbox
+              value={object('Checkbox values', store.state)}
+              size={select<any>('Size', { small: 'small', big: 'big' }, 'small')}
+              multiple={boolean('Is multiple?', false)}
+              disabled={boolean('Disabled', false)}
+              onChange={onChange}
+              type={'light'}
+            >
+              <div data-key='yt'>YouTube</div>
+              <div data-key='tw'>Twitter</div>
+              <div data-key='inst'>Instagram Instagram Instagram Instagram Instagram</div>
+            </Checkbox>
+          </ControlWrapper>
+        </div>
+        <div style={{ width: '310px', background: '#4B4C5C', padding: '30px' }}>
+          <ControlWrapper
+            text={text('Title', 'Enable ability to give tips on ‘Like’ posts')}
+            type={'dark'}
+            disabled={boolean('Disabled', false)}
+          >
+            <Checkbox
+              value={object('Checkbox values', store.state)}
+              size={select<any>('Size', { small: 'small', big: 'big' }, 'small')}
+              multiple={boolean('Is multiple?', false)}
+              disabled={boolean('Disabled', false)}
+              onChange={onChange}
+              type={'dark'}
+            >
+              <div data-key='yt'>YouTube</div>
+              <div data-key='tw'>Twitter</div>
+              <div data-key='inst'>Instagram Instagram Instagram Instagram Instagram</div>
+            </Checkbox>
+          </ControlWrapper>
+        </div>
       </>
     )
   }))
   .add('Radio', withState({ '5': true, '10': false, '15': false, '20': false }, (store) => {
-    const onChange = (key: string, selected: boolean, child: React.ReactNode, all: {[key: string]: boolean}) => {
+    const onChange = (key: string, selected: boolean, child: React.ReactNode, all: { [key: string]: boolean }) => {
       store.set(all)
     }
     return (
       <>
-       <div style={{ width: '295px', background: '#fff', padding: '30px 60px' }}>
-        <ControlWrapper
-          text={text('Title', 'Monthly Budget')}
-          type={'light'}
-          disabled={boolean('Disabled', false)}
-        >
-          <Radio
-            value={object('Radio values', store.state)}
-            size={select('Size', { small: 'small', big: 'big' }, 'small')}
+        <div style={{ width: '295px', background: '#fff', padding: '30px 60px' }}>
+          <ControlWrapper
+            text={text('Title', 'Monthly Budget')}
+            type={'light'}
             disabled={boolean('Disabled', false)}
-            onChange={onChange}
           >
-            <div data-value='5'>
-              <Tokens
-                size={'mini'}
-                value={'5'}
-                data-value={'5'}
-                color={'donation'}
-              />
-            </div>
-            <div data-value='10'>
-              <Tokens
-                size={'mini'}
-                value={'10'}
-                data-value={'10'}
-                color={'donation'}
-              />
-            </div>
-            <div data-value='15'>
-              <Tokens
-                size={'mini'}
-                value={'15'}
-                data-value={'15'}
-                color={'donation'}
-              />
-            </div>
-            <div data-value='20'>
-              <Tokens
-                size={'mini'}
-                value={'20'}
-                data-value={'20'}
-                color={'donation'}
-              />
-            </div>
-          </Radio>
-        </ControlWrapper>
-      </div>
+            <Radio
+              value={object('Radio values', store.state)}
+              size={select<any>('Size', { small: 'small', big: 'big' }, 'small')}
+              disabled={boolean('Disabled', false)}
+              onChange={onChange}
+            >
+              <div data-value='5'>
+                <Tokens
+                  size={'mini'}
+                  value={'5'}
+                  data-value={'5'}
+                  color={'donation'}
+                />
+              </div>
+              <div data-value='10'>
+                <Tokens
+                  size={'mini'}
+                  value={'10'}
+                  data-value={'10'}
+                  color={'donation'}
+                />
+              </div>
+              <div data-value='15'>
+                <Tokens
+                  size={'mini'}
+                  value={'15'}
+                  data-value={'15'}
+                  color={'donation'}
+                />
+              </div>
+              <div data-value='20'>
+                <Tokens
+                  size={'mini'}
+                  value={'20'}
+                  data-value={'20'}
+                  color={'donation'}
+                />
+              </div>
+            </Radio>
+          </ControlWrapper>
+        </div>
       </>
     )
   }))
@@ -241,8 +241,8 @@ storiesOf('Components/Form controls', module)
     return (
       <div style={{ width: '250px' }}>
         <ControlWrapper
-            text={text('Title', 'Recovery keys')}
-            disabled={boolean('Disabled', false)}
+          text={text('Title', 'Recovery keys')}
+          disabled={boolean('Disabled', false)}
         >
           <TextArea
             value={text('Value', store.state.value)}
@@ -261,8 +261,8 @@ storiesOf('Components/Form controls', module)
     return (
       <div style={{ width: '500px' }}>
         <ControlWrapper
-            text={text('Title', 'Recovery keys')}
-            disabled={boolean('Disabled', false)}
+          text={text('Title', 'Recovery keys')}
+          disabled={boolean('Disabled', false)}
         >
           <TextAreaClipboard
             copiedString='Copied!'
@@ -280,7 +280,7 @@ storiesOf('Components/Form controls', module)
       store.set({ value: event.target.value })
     }
     const typeOptions = { text: 'text', email: 'email', search: 'search', password: 'password' }
-    const type = select('Type', typeOptions, 'text')
+    const type = select<any>('Type', typeOptions, 'text')
     return (
       <Input
         type={type}
