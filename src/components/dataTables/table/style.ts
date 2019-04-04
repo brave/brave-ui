9245ceeb7fee99717ae6412ceaf06b359a78b6a0
Ross Moody <rmoody@brave.com>
2019-04-03 17:06:28 -0700

    style updates
    tweaks
    
    snapshots
    
    select update
    
    snapshots

--- src/components/dataTables/table/style.ts
+++ src/components/dataTables/table/style.ts
@@ -2,7 +2,7 @@
  * License. v. 2.0. If a copy of the MPL was not distributed with this file.
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import styled, { css } from 'styled-components'
+import styled, { css } from '../../../theme'
 import { Cell, Row } from './index'
 
 export const StyledNoContent = styled<{}, 'div'>('div')`
@@ -13,21 +13,20 @@ export const StyledNoContent = styled<{}, 'div'>('div')`
 `
 
 export const StyledTable = styled<{}, 'table'>('table')`
-  width: 100%;
-  margin-bottom: 20px;
+  min-width: 100%;
+  margin: 24px 0;
 `
 
 export const StyledTH = styled<Partial<Cell>, 'th'>('th')`
   text-transform: uppercase;
   text-align: left;
-  font-family: Poppins, sans-serif;
+  font-family: ${p => p.theme.fontFamily.body};
   font-size: 12px;
   font-weight: 500;
-  line-height: 2.33;
   border-bottom: 2px solid #dedfe4;
   border-top: 2px solid #dedfe4;
   color: #4B4C5C;
-  padding: 9px 0;
+  padding: 12px 0;
 
   ${p => p.customStyle
     ? css`
@@ -50,12 +49,10 @@ export const StyledTD = styled<Partial<Cell>, 'td'>('td')`
   font-family: Muli, sans-serif;
   font-size: 14px;
   font-weight: 500;
-  line-height: 1.29;
   color: #686978;
   border-bottom: solid 1px #e4e8ec;
-  padding: 9px 0;
+  padding: 12px 0;
   text-align: left;
-  height: 42px;
 
   ${p => p.customStyle
     ? css`
