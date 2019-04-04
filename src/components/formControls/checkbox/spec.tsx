fc0d5aeaf3577d6c4c62184a3ccf9038f564b3bf
Ross Moody <rmoody@brave.com>
2019-04-03 17:05:20 -0700

    snapshots

--- src/components/formControls/checkbox/spec.tsx
+++ src/components/formControls/checkbox/spec.tsx
@@ -2,18 +2,22 @@
 import * as React from 'react'
 import { shallow } from 'enzyme'
 import { create } from 'react-test-renderer'
-import Checkbox, {Props} from './index'
+import Checkbox, { Props } from './index'
+import { TestThemeProvider } from '../../../theme'
 
 describe('Checkbox tests', () => {
-  const baseComponent = (props?: Props) => <Checkbox
-    value={{'yt': true, 'tw': false, 'inst': false}}
-    id={'checkbox'}
-    multiple={false}
-  >
-    <div data-key='yt'>YouTube</div>
-    <div data-key='tw'>Twitter</div>
-    <div data-key='inst'>Instagram</div>
-  </Checkbox>
+  const baseComponent = (props?: Props) =>
+    <TestThemeProvider>
+      <Checkbox
+        value={{ 'yt': true, 'tw': false, 'inst': false }}
+        id={'checkbox'}
+        multiple={false}
+      >
+        <div data-key='yt'>YouTube</div>
+        <div data-key='tw'>Twitter</div>
+        <div data-key='inst'>Instagram</div>
+      </Checkbox>
+    </TestThemeProvider>
 
   describe('basic tests', () => {
     it('matches the snapshot', () => {
