3a0f21aaaddaea7403732b518ece368fa5b2ebf5
Cezar Augusto <boss@cezaraugusto.net>
2019-06-05 11:45:44 -0300

    Undo revert of https://github.com/brave/brave-ui/pull/485 Revert "Merge pull request #490 from brave/revert-485"
    This reverts commit 82b3e981040b2ba89743108e6701f5fa24ce3b52, reversing
    changes made to b0ec08cd4ed2a20401d058062f1c238fb9a67813.

--- src/theme/brave-dark.ts
+++ src/theme/brave-dark.ts
@@ -1,9 +1,15 @@
 import ITheme from './theme-interface'
+import colors from './colors'
 import defaultTheme from './brave-default'
 
 const darkTheme: ITheme = {
   ...defaultTheme,
-  name: 'Brave Dark'
+  name: 'Brave Dark',
+  color: {
+    ...defaultTheme.color,
+    contextMenuBackground: colors.black,
+    contextMenuForeground: colors.white
+  }
 }
 
 export default darkTheme
