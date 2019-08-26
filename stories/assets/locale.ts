a80719b4ff8d878ef6fe85de29b22ff73ed7b672
NejcZdovc <nejc.zdovc@3zsistemi.si>
2019-08-26 16:34:26 +0200

    Adds new option for publisher status in the panel

--- stories/assets/locale.ts
+++ stories/assets/locale.ts
@@ -44,6 +44,7 @@ const locale: Record<string, string> = {
   captchaMissedTarget: 'Hmm… Not Quite. Try Again.',
   claim: 'Claim',
   closeBalance: 'Closing Balance',
+  connectedText: 'This creator has not yet signed up to receive contributions from Brave users. Any tips you send will remain in your wallet until they verify.',
   contribute: 'Contribute',
   contributeAllocation: 'Auto-Contribute Allocation',
   contributeTooltip: 'Auto-Contribute Settings',
