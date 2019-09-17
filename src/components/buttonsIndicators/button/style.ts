1af1ce11338569f759d24c887be548ce0f4467b3
Anton Lazarev <antonok35@gmail.com>
2019-09-17 14:59:20 -0700

    update button styles to match mockups

--- src/components/buttonsIndicators/button/style.ts
+++ src/components/buttonsIndicators/button/style.ts
@@ -21,51 +21,35 @@ function largeMediumSmall (largeValue: any, mediumValue: any, smallValue: any, c
 }
 
 const getThemeColors = (p: ThemedStyledProps<Props>) => {
-  let mainColor
+  let subtleBackgroundColor
   let hoverColor
-  let activeColor
-  if (p.disabled) {
-    mainColor = hoverColor = activeColor = p.theme.color.disabled
-  } else {
-    switch (p.type) {
-      case 'accent':
-        if (p.brand === 'brave') {
-          mainColor = p.theme.color.brandBrave
-          hoverColor = p.theme.color.brandBraveInteracting
-          activeColor = p.theme.color.brandBraveActive
-        } else if (p.brand === 'rewards') {
-          mainColor = p.theme.color.brandBat
-          hoverColor = p.theme.color.brandBatInteracting
-          activeColor = p.theme.color.brandBatActive
-        }
-        break
-      case 'default':
-        mainColor = p.theme.color.defaultControl
-        hoverColor = p.theme.color.defaultControlInteracting
-        activeColor = p.theme.color.defaultControlActive
-        break
-      case 'warn':
-        mainColor = p.theme.color.warn
-        hoverColor = p.theme.color.warnInteracting
-        activeColor = p.theme.color.warnActive
-        break
-      case 'subtle':
-        mainColor = p.theme.color.subtle
-        hoverColor = p.theme.color.subtleInteracting
-        activeColor = p.theme.color.subtleActive
-        break
-    }
+  let mainColor
+  let pressedColor
+  let outlineColor
+  if (p.brand === 'brave') {
+    subtleBackgroundColor = p.theme.color.brandBraveSubtleBackground
+    hoverColor = p.theme.color.brandBraveHover
+    mainColor = p.theme.color.brandBraveMain
+    pressedColor = p.theme.color.brandBravePressed
+    outlineColor = p.theme.color.brandBraveOutline
+  } else if (p.brand === 'rewards') {
+    subtleBackgroundColor = p.theme.color.brandBatSubtleBackground
+    hoverColor = p.theme.color.brandBatHover
+    mainColor = p.theme.color.brandBatMain
+    pressedColor = p.theme.color.brandBatPressed
+    outlineColor = p.theme.color.brandBatOutline
   }
   return css`
-    --button-main-color: ${mainColor};
-    --button-main-color-hover: ${hoverColor};
-    --button-main-color-active: ${activeColor};
+    --brand-color-subtle: ${subtleBackgroundColor};
+    --brand-color-hover: ${hoverColor};
+    --brand-color-main: ${mainColor};
+    --brand-color-pressed: ${pressedColor};
+    --brand-style-focus-shadow: 0 0 0 4px ${outlineColor};
   `
 }
 
 const StyledButton = styled<Props, 'button'>('button')`
   ${getThemeColors}
-  --button-state-color: var(--button-main-color);
   --icon-size: ${largeMediumSmall('18px', '16px', '14px')};
   --icon-spacing: ${largeMediumSmall('6px', '6px', '6px')};
   --webkit-appearance: none;
@@ -77,37 +61,87 @@ const StyledButton = styled<Props, 'button'>('button')`
   flex-direction: ${p => p.icon && p.icon.position === 'after' ? 'row' : 'row-reverse'};
   justify-content: center;
   align-items: center;
-  font-family: Poppins, sans-serif;
+  font-family: Muli, sans-serif;
   cursor: ${p => p.disabled ? 'default' : 'pointer'};
   user-select: none;
-  font-size: ${largeMediumSmall('14px', '13px', '11px')};
+  font-size: ${largeMediumSmall('16px', '14px', '13px')};
   border-radius: ${largeMediumSmall('24px', '20px', '16px', '28px')};
   width: ${p => p.size === 'call-to-action' ? '100%' : 'auto'};
-  min-width: ${largeMediumSmall('116px', '104px', '88px', '235px')};
-  padding: ${largeMediumSmall('14px 15px', '11px 15px', '7px 10px', '19px 15px')};
-  :hover:enabled {
-    --button-state-color: var(--button-main-color-hover);
-  }
-  :active:enabled {
-    --button-state-color: var(--button-main-color-active);
-  }
+  min-width: ${largeMediumSmall('85px', '82px', '68px', '235px')};
+  padding: ${largeMediumSmall('13px 20px', '9px 16px', '5px 16px', '19px 15px')};
+  opacity: ${p => p.disabled ? '0.4' : 'none'};
 `
 
 export default StyledButton
 
+export const DefaultButton = styled(StyledButton)`
+  color: ${p => p.theme.color.defaultControl};
+  border: 1px solid ${p => p.theme.color.defaultButtonBorder};
+  :hover:enabled {
+    color: var(--brand-color-main);
+    border-color: var(--brand-color-main);
+  }
+  :focus:enabled {
+    border-color: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
+  :active:enabled {
+    color: var(--brand-color-main);
+    border-color: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
+`
+
 export const PrimaryButton = styled(StyledButton)`
   color: #fff;
-  background: var(--button-state-color);
-  border: 1px solid var(--button-state-color);
+  background: var(--brand-color-main);
+  :hover:enabled {
+    background: var(--brand-color-hover);
+  }
+  :focus:enabled {
+    background: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
+  :active:enabled {
+    background: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
 `
 
 export const SecondaryButton = styled(StyledButton)`
-  border: 1px solid;
-  color: var(--button-state-color);
+  color: var(--brand-color-main);
+  :hover:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-main);
+  }
+  :focus:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
+  :active:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-pressed);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
 `
 
 export const TertiaryButton = styled(StyledButton)`
-  color: var(--button-state-color);
+  color: ${p => p.theme.color.defaultControl};
+  :hover:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-main);
+  }
+  :focus:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-main);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
+  :active:enabled {
+    background: var(--brand-color-subtle);
+    color: var(--brand-color-pressed);
+    box-shadow: var(--brand-style-focus-shadow);
+  }
 `
 
 export const StyledText = styled<Props, 'div'>('div')`
@@ -117,7 +151,7 @@ export const StyledText = styled<Props, 'div'>('div')`
   align-items: center;
   text-align: center;
   letter-spacing: 0;
-  font-weight: 500;
+  font-weight: ${p => p.level === 'primary' ? 700 : 600};
   text-transform: ${p => p.size === 'call-to-action' ? 'uppercase' : 'none'};
   line-height: 1;
 `
