a7a6f54c423ff38d7f81a58a3ddef2788268c440
Douglas Daniel <douglashdaniel@gmail.com>
2021-04-21 17:25:59 -0600

    Added new colors to theme

--- src/theme/brave-dark.ts
+++ src/theme/brave-dark.ts
@@ -7,15 +7,33 @@ const darkTheme: ITheme = {
   name: 'Brave Dark',
   color: {
     ...defaultTheme.color,
-    contextMenuBackground: colors.black,
-    contextMenuForeground: colors.white,
-    contextMenuHoverBackground: colors.blurple500,
-    contextMenuHoverForeground: colors.white,
+    // brand
+    interactive07: colors.grey000,
+    interactive08: colors.grey700,
+    // regular controls
     defaultControl: colors.grey400,
     defaultControlInteracting: colors.white,
     defaultControlActive: colors.grey500,
+    // text
+    contextMenuForeground: colors.white,
+    contextMenuHoverForeground: colors.white,
+    text: colors.white,
+    text01: colors.grey000,
+    text02: colors.grey400,
+    text03: colors.grey600,
+    // backgrounds
+    contextMenuBackground: colors.black,
+    contextMenuHoverBackground: colors.blurple500,
     panelBackground: colors.grey900,
-    text: colors.white
+    background01: '#17171F',
+    background02: colors.grey900,
+    // dividers and outlines
+    divider01: colors.grey800,
+    // alerts
+    errorText: '#FF4B6A',
+    errorBackground: 'rgba(227, 36, 68, 0.2)',
+    warningBackground: 'rgba(255, 221, 99, 0.2)',
+    successBackground: 'rgba(81, 207, 102, 0.3)'
   }
 }
 
--- src/theme/brave-default.ts
+++ src/theme/brave-default.ts
@@ -10,6 +10,16 @@ const theme: ITheme = {
     ...colors
   },
   color: {
+    // brand
+    interactive01: '#EA3A0D',
+    interactive02: colors.orange500,
+    interactive03: colors.orange400,
+    interactive04: colors.blurple600,
+    interactive05: colors.blurple500,
+    interactive06: colors.blurple400,
+    interactive07: colors.neutral900,
+    interactive08: colors.grey500,
+    focusBorder: colors.blurple300,
     brandBrave: colors.orange500,
     brandBat: colors.blurple500,
     // brave controls
@@ -44,16 +54,38 @@ const theme: ITheme = {
     contextMenuHoverBackground: colors.blurple200,
     secondaryBackground: colors.grey400,
     modalOverlayBackground: 'rgba(36,37,54,0.85)',
+    background01: colors.neutral000,
+    background02: colors.white,
+    background03: colors.grey800,
     // text
     contextMenuForeground: colors.grey800,
     contextMenuHoverForeground: colors.neutral800,
     detailDescription: colors.grey500,
     text: colors.grey700,
+    text01: colors.neutral900,
+    text02: colors.neutral700,
+    text03: colors.neutral600,
     // form controls
     inputBorder: colors.grey500,
     separatorLine: colors.grey100,
     // images
-    lionLogo: colors.grey500
+    lionLogo: colors.grey500,
+    // dividers and outlines
+    divider01: '#E9E9F4',
+    // alerts
+    errorText: colors.red600,
+    errorIcon: colors.red600,
+    errorBorder: colors.red500,
+    errorBackground: colors.red000,
+    warningIcon: colors.yellow500,
+    warningBorder: colors.yellow400,
+    warningBackground: colors.yellow000,
+    infoIcon: colors.blue500,
+    infoBorder: colors.blue400,
+    infoBackground: 'rgba(93, 181, 252, 0.2)',
+    successIcon: colors.teal600,
+    successBorder: colors.green500,
+    successBackground: colors.green200
   },
   fontFamily: {
     heading: 'Poppins, sans-serif',
