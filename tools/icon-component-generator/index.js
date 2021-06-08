1a6fcdfd8dd3d31b3df47dd74af929b698aca7fa
Jacob Lamont <jlamont@brave.com>
2021-06-08 17:08:32 -0400

    adds v2 icons to story and generator
    - modifies icon generation script to also build an SVG sprite
    - adds stories for v2 icons, and icon sprite
    - updates dependencies to avoid NPM audit security issues

--- tools/icon-component-generator/index.js
+++ tools/icon-component-generator/index.js
@@ -1,12 +1,12 @@
 const fs = require('mz/fs')
 const path = require('path')
-const SVGO = require('svgo')
-const svg2jsx = require('@balajmarius/svg2jsx')
-const mkdirp = require('mkdirp-promise')
+const { optimize } = require('svgo')
+const mkdirp = require('mkdirp')
 const { prettyPrint } = require('html')
 const findFilesRecursive = require('./util/findFilesRecursive')
 const fixLint = require('./util/fixLint')
 const template = require('./template')
+const spriteTemplate = require('./spriteTemplate')
 const svgoConfig = require('./svgoConfig')
 
 const inputPath = path.resolve(process.argv[2])
@@ -15,18 +15,27 @@ const outputPath = path.resolve(process.argv[3])
 const moduleExportLines = [ ]
 
 function PascalCaseDashedString (dashedString) {
-  dashedString = dashedString.replace(/-([a-z])/g, g => g[1].toUpperCase())
+  dashedString = dashedString.replace(/(\_|\-)([a-z])/g, g => g[1].toUpperCase())
   return dashedString[0].toUpperCase() + dashedString.slice(1)
+}
+
+function camelCaseString (string) {
+  return string
+    .replace("_", "-")
+    .replace(/(\S)([A-Z])/, "$1-$2")
+    .toLowerCase()
 }
 
 async function optimizeAndSave (filePath) {
   const data = await fs.readFile(filePath, 'utf8')
-  const optimized = await svgo.optimize(data)
+  const optimized = await optimize(data, svgoConfig)
   const relativeFilePathNoExt = filePath.replace(inputPath, '').replace(path.extname(filePath), '')
   const savedRelativePath = relativeFilePathNoExt + '.tsx'
   const savedPathName = path.join(outputPath, savedRelativePath)
   await mkdirp(path.dirname(savedPathName))
-  let transformed = prettyPrint(await svg2jsx(optimized.data))
+  let transformed = optimized.data
+  transformed = transformed.replace(/(import|export).*?;/g, "")
+  transformed = prettyPrint(transformed)
   transformed = template.prefix + transformed.replace(/\"/g, `'`) + template.suffix + require('os').EOL
   transformed = await fixLint(transformed)
   // generate: export { default as CheckIcon }  from './check'
@@ -35,17 +44,49 @@ async function optimizeAndSave (filePath) {
   return savedPathName
 }
 
+async function generateSprite (filePaths) {
+  const { plugins, ...initialConfig } = svgoConfig;
+
+  const symbols = await Promise.all(filePaths.map(async filePath => {
+    const data = await fs.readFile(filePath, 'utf8')
+    
+    const relativeFilePathNoExt = `${filePath.replace(inputPath, '').replace(path.extname(filePath), '')}Icon`
+    const iconName = camelCaseString(path.basename(relativeFilePathNoExt))
+
+    const config = { ...initialConfig, plugins: [...plugins, 'removeXMLNS'] }
+    const optimized = await optimize(data, { ...config })
+    const symbol = optimized.data
+      .replace("<svg", `<symbol id="${iconName}"`)
+      .replace("</svg>", "</symbol>")
+    return symbol
+  }));
+
+  const spriteSVG = `<svg xmlns="http://www.w3.org/2000/svg" style="width:0;height:0;position:absolute;">
+  ${symbols.join(require('os').EOL)}
+</svg>`
+
+  const removeUselessDefsIdx = plugins.indexOf('removeUselessDefs')
+  const spritePlugins = [...plugins].splice(removeUselessDefsIdx, 1)
+  const config = { ...initialConfig, plugins: spritePlugins }
+  const optimizedSprite = optimize(spriteSVG, config)
+  const spriteFile = spriteTemplate.prefix + optimizedSprite.data + spriteTemplate.suffix
+
+  const spriteFilePath = "../../stories/components/icons/brave-icons-svg.js"
+  await fs.writeFile(spriteFilePath, spriteFile, 'utf8')
+}
+
 async function doScript () {
   console.log(`Finding SVG files to convert at path: "${inputPath}"`)
   const paths = await findFilesRecursive('svg', inputPath)
   console.log(`Found ${paths.length} files. Converting...`)
   const savedPaths = await Promise.all(paths.map(optimizeAndSave))
   console.log(savedPaths)
+
+  await generateSprite(paths);
+
   for (const exportLine of moduleExportLines) {
     console.log(exportLine)
   }
 }
 
 doScript()
-
-const svgo = new SVGO(svgoConfig)
